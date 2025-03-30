aba2f778201e34c9a4aea89eec94e903c6be4799
Oscar Marcos <oscarmarcos2673@gmail.com>
2025-03-30 13:38:10 +0200

    added some global configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: "#334559",
+          200: "#2E3C4C",
+          300: "#1E2631",
+        },
+        primary: {
+          50: "#f1f9fe",
+          100: "#e2f1fc",
+          200: "#bee4f9",
+          300: "#85cef4",
+          400: "#38b0eb",
+          500: "#1b9cdc",
+          600: "#0e7cbb",
+          700: "#0d6397",
+          800: "#0f557d",
+          900: "#124668",
+          950: "#0c2d45",
+        },
+      },
+    },
   },
   plugins: [],
 };
