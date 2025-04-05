555b4bc6bae1a4cc44f083de0cda087a1879b908
Oscar Marcos <oscarmarcos2673@gmail.com>
2025-04-05 21:48:56 +0200

    completado en la version movil el login que te lleva correctamente a la ruta adecuada dependiendo de si estas logeado y que tipo de role tienes

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,38 @@
 import { Redirect } from "expo-router";
+import { ERole } from "models/api";
+import { useEffect, useState } from "react";
+import { useAuthStore } from "store/auth.store";
 
 export default function Index() {
-  return <Redirect href="/(auth)" />;
-} 
+  const [isLoading, setIsLoading] = useState(true);
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const role = useAuthStore((state) => state.role);
+
+  const privateContainerToRender = () => {
+    if (role === ERole.SUPERADMIN) {
+      return <Redirect href="/(admin)" />;
+    } else {
+      return <Redirect href="/(tabs)" />;
+    }
+  };
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      try {
+        await useAuthStore.getState().initializeAuth();
+      } catch (error) {
+        console.error("Error initializing auth:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    initializeAuth();
+  }, []);
+
+  if (isLoading) {
+    return null; // O puedes retornar un componente de carga
+  }
+
+  return isLoggedIn ? privateContainerToRender() : <Redirect href="/(auth)" />;
+}
